7375fd2d038e1b403da14a8b793404d7a507be08
MohgaMA <mohga.m.ashour@gmail.com>
2023-10-17 10:32:32 -0700

    update for task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    l = len(my_list)
-    if idx < 0 or idx > (l - 1):
-        return(None)
-    for i in range(l):
-        if idx == i:
-            return(my_list[i])
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return (my_list[idx])
