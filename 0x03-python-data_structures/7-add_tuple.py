e4a9700187af05bb6fc06c603cb08fead093166e
MohgaMA <mohga.m.ashour@gmail.com>
2023-10-23 12:10:51 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tuple_a)
     len_b = len(tuple_b)
 
-    if (len_a == 0):
-        new = (tuple_b)
-    elif (len_b == 0):
-        new = (tuple_a)
-    elif (len_a < 2):
+    if (len_a < 2):
         new = ((tuple_a[0] + tuple_b[0]), (0 + tuple_b[1]))
     elif (len_b < 2):
         new = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0))
