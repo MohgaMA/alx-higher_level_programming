8e04452dba889fa88fdcbe2a3577073458e15eb1
MohgaMA <mohga.m.ashour@gmail.com>
2023-10-23 13:32:29 -0700

    7 6 5 4 3 file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except (FloatingPointError, ZeroDivisionError):
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    tmp_result = 0
+    for i in range(0, list_length):
+        try:
+            tmp_result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            tmp_result = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            tmp_result = 0
+            print("division by 0")
+        except IndexError:
+            tmp_result = 0
+            print("out of range")
+        finally:
+            pass
+        result.append(tmp_result)
     return result
